1c9eb79ce045581372c9f850d754adc63e755647
BugCatcherNate <natetthompson@gmail.com>
2022-11-05 10:51:49 -0500

    added ortho camera and camera movement

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::camera::ScalingMode};
 
 fn main() {
     App::new()
@@ -10,40 +10,37 @@ fn main() {
 
 fn cube_movement(
     keyboard_input: Res<Input<KeyCode>>,
-    mut head_positions: Query<&mut Transform, With<Cube>>,
-    time: Res<Time>
+    mut head_positions: Query<&mut Transform, With<Camera>>,
+    time: Res<Time>,
 ) {
+    let cameraSpeed = 5.0;
     for mut transform in head_positions.iter_mut() {
         if keyboard_input.pressed(KeyCode::A) {
-            transform.translation.x -= 1.0 * time.delta_seconds();
+            transform.translation.x -= cameraSpeed * time.delta_seconds();
         }
         if keyboard_input.pressed(KeyCode::D) {
-
-            transform.translation.x += 1.0 * time.delta_seconds();
+            transform.translation.x += cameraSpeed * time.delta_seconds();
         }
         if keyboard_input.pressed(KeyCode::W) {
-
-            transform.translation.z -= 1.0 * time.delta_seconds();
+            transform.translation.z -= cameraSpeed * time.delta_seconds();
         }
         if keyboard_input.pressed(KeyCode::S) {
-            
-            transform.translation.z += 1.0 * time.delta_seconds();
+            transform.translation.z += cameraSpeed * time.delta_seconds();
         }
         if keyboard_input.pressed(KeyCode::Space) {
-            
-            transform.translation.y += 1.0 * time.delta_seconds();
+            transform.translation.y += cameraSpeed * time.delta_seconds();
         }
-     if keyboard_input.pressed(KeyCode::LShift) {
-            
-            transform.translation.y -= 1.0 * time.delta_seconds();
+        if keyboard_input.pressed(KeyCode::LShift) {
+            transform.translation.y -= cameraSpeed * time.delta_seconds();
         }
-
     }
 }
 
 #[derive(Component)]
 struct Cube;
 
+#[derive(Component)]
+struct Camera;
 
 /// set up a simple 3D scene
 fn setup(
@@ -53,35 +50,56 @@ fn setup(
 ) {
     // plane
     commands.spawn_bundle(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 10.0 })),
         material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
         ..default()
     });
     // cube
-    commands.spawn_bundle(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-        transform: Transform {
-            translation: Vec3::new(0.0, 0.0, 0.0),
-        ..default()
-        }, ..default()
-    }).insert(Cube);
+    commands
+        .spawn_bundle(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+            material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+            transform: Transform {
+                translation: Vec3::new(0.0, 0.0, 0.0),
+                ..default()
+            },
+            ..default()
+        })
+        .insert(Cube);
     // light
-    commands.spawn_bundle(PointLightBundle {
-        point_light: PointLight {
-            intensity: 1500.0,
+    const HALF_SIZE: f32 = 10.0;
+    commands.spawn_bundle(DirectionalLightBundle {
+        directional_light: DirectionalLight {
+            shadow_projection: OrthographicProjection {
+                left: -HALF_SIZE,
+                right: HALF_SIZE,
+                bottom: -HALF_SIZE,
+                top: HALF_SIZE,
+                near: -10.0 * HALF_SIZE,
+                far: 10.0 * HALF_SIZE,
+                ..default()
+            },
             shadows_enabled: true,
             ..default()
         },
         transform: Transform {
-
-        ..default()
+            translation: Vec3::new(0.0, 2.0, 0.0),
+            ..default()
         },
         ..default()
     });
     // camera
-    commands.spawn_bundle(Camera3dBundle {
-        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..default()
-    });
+
+    commands
+        .spawn_bundle(Camera3dBundle {
+            projection: OrthographicProjection {
+                scale: 3.0,
+                scaling_mode: ScalingMode::FixedVertical(2.0),
+                ..default()
+            }
+            .into(),
+            transform: Transform::from_xyz(5.0, 5.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+            ..default()
+        })
+        .insert(Camera);
 }
